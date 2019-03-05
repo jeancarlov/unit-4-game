a768f4ffe3a0937fa91ca7b9b717fc1bbb5ce330
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-03-05 15:50:46 -0500

    Done, the targetpick that was call at the begining was creating some error since it was declared as global variable. Instead, I move the random assign for the target pick and card pick to the initializer.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,19 @@
 //JavaScript funtion that wraps everything
 $(document).ready(function () {
 
-    // computerPick and userPick bank place holders
-
-    var targetPick = Math.floor (Math.random () *119)+1;
-    $('#target-pick').text(targetPick);
-
-    // assigns random number to each image
-    $(".card-1").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
-    $(".card-2").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
-    $(".card-3").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
-    $(".card-4").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
-
 
     //list of Global variables to the runtime of the application
     var wins = 0;
     var losses= 0;
     var totalScore = 0;
+    var targetPick;
 
     //wins and loses declaration on the html
     $('#number-wins').text(wins);
     $('#number-losses').text(losses);
     $('#total-score').text(totalScore);
 
-
-    // random number assign to each card
+    initializer();
 
 
 //    // Here we created an on-click event that responds to button clicks of the crystal image.
@@ -36,6 +25,15 @@ $(document).ready(function () {
         // crystalValue = parseInt(crystalValue);
         totalScore = totalScore + crystalValue;
         $("#total-score").text(totalScore);
+
+        if(targetPick === totalScore){
+            youWin();
+            
+        } else if (targetPick < totalScore){
+           youLose();
+           
+        }
+    
         });
 
 
@@ -43,11 +41,17 @@ $(document).ready(function () {
 
     //  This functio will initialize the game.
     function initializer() {
-        targetPick = ''; // should this be in quotes
-        userPick = 0;
-        console.log(userPick)
+         targetPick = Math.floor (Math.random () *119)+1;
+        $('#target-pick').text(targetPick);
+    
+        // assigns random number to each image
+        $(".card-1").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
+        $(".card-2").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
+        $(".card-3").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
+        $(".card-4").attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
+    
         totalScore = 0
-        // $('#total-score').text(totalScore);
+        $('#total-score').text(totalScore);
         
     }
 
